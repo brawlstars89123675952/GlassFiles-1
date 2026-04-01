e97352ce2ab661f063aa2611a9bf5434f7e7a1f7
brawlstars89123675952 <brawlstars89123675952@gmail.com>
2026-04-01 04:46:27 +0300

    Fix repo settings aliases for Compose layout types

---
+++ app/src/main/java/com/glassfiles/ui/screens/GitHubRepoSettingsAliases.kt
@@ -0,0 +1,4 @@
+package com.glassfiles.ui.screens
+
+typealias PaddingValues = androidx.compose.foundation.layout.PaddingValues
+typealias ColumnScope = androidx.compose.foundation.layout.ColumnScope
